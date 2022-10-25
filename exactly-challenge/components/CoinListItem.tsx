6cef1b8d8cbb51e1b34a284a5d1200d365f747b0
Luciano Bianchi <lucianogbianchi@gmail.com>
2022-10-25 00:07:03 -0700

    Add barebones coin detail page

--- exactly-challenge/components/CoinListItem.tsx
+++ exactly-challenge/components/CoinListItem.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
 import { Market } from "../services/CoinGecko/coins";
 
 export const CoinListItem: React.FC<{ market: Market }> = ({ market }) => {
-    const { name, current_price } = market;
+    const { name, current_price, id } = market;
 
     return (
-        <div>
-            {name} - ${current_price}
-        </div>
+        <Link href={`/coins/${id}`}>
+            <a>
+                {name} - ${current_price}
+            </a>
+        </Link>
     );
 };
---
+++ exactly-challenge/pages/coins/[coinId].tsx
@@ -0,0 +1,83 @@
+import { NextPage, NextPageContext } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import {
+    getMarketChart,
+    MarketChartResponse,
+} from "../../services/CoinGecko/coins";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+
+const timeframes = ["1d", "1m", "6m", "1y", "5y"] as const;
+export type ChartTimeframes = typeof timeframes[number];
+
+const timeframesToDaysAgo: { [tf in ChartTimeframes]: number } = {
+    "1d": 1,
+    "1m": 30,
+    "6m": 180,
+    "1y": 360,
+    "5y": 1800,
+};
+
+export const getServerSideProps = async (context: NextPageContext) => {
+    const { coinId } = context.query;
+
+    const markets = await getMarketChart(coinId as any, 1);
+    // TODO: error handling
+    return {
+        props: {
+            initialMarketChart: markets.data,
+        },
+    };
+};
+
+const CoinPage: NextPage<{ initialMarketChart: MarketChartResponse }> = ({
+    initialMarketChart,
+}) => {
+    const router = useRouter();
+    const { coinId, tf = "1d" } = router.query;
+
+    const [chartTf, setChartTf] = useState<ChartTimeframes>(
+        tf as ChartTimeframes
+    );
+
+    const handleAlignment = (
+        event: React.MouseEvent<HTMLElement>,
+        newTimeframe?: ChartTimeframes
+    ) => {
+        event.preventDefault();
+        if (newTimeframe) {
+            setChartTf(newTimeframe);
+        }
+    };
+
+    const [marketChart, setMarketChart] =
+        useState<MarketChartResponse>(initialMarketChart);
+
+    useEffect(() => {
+        getMarketChart(coinId as string, timeframesToDaysAgo[chartTf]).then(
+            (res) => setMarketChart(res.data)
+        );
+    }, [coinId, chartTf]);
+
+    console.log(marketChart);
+
+    return (
+        <div>
+            <p>Coin: {coinId}</p>
+            <ToggleButtonGroup
+                value={chartTf}
+                exclusive
+                onChange={handleAlignment}
+            >
+                {timeframes.map((timeframe) => (
+                    <ToggleButton value={timeframe} key={timeframe}>
+                        {timeframe}
+                    </ToggleButton>
+                ))}
+            </ToggleButtonGroup>
+        </div>
+    );
+};
+
+export default CoinPage;
